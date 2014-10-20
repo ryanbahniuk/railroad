7797d91b780ffbc4d5eb4514521b0b0114f076d7
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2014-10-19 19:54:47 -0500

    adding user model

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User
+	include Mongoid::Document
+	field :name, type: String
+	field :email, type: String
+	field :password_digest, type: String
+
+	def authenticate(password)
+		self.password == password
+	end
+
+	def password
+		@password ||= BCrypt::Password.new(password_digest)
+	end
+
+	def password=(pass)
+		@password = BCrypt::Password.create(pass)
+		self.password_digest = @password
+	end
+end
