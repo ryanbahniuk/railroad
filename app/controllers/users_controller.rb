b48190bfc44773034547f96a496f0e7fca791060
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2014-10-19 21:47:20 -0500

    cru for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,26 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		redirect_to root_path unless current_user.id == @user.id
+
+		if @user.update(update_user_params)
+			redirect_to root_path
+		else
+			render 'edit'
+		end
+	end
+
 	private
+
+	def update_user_params
+		params.require(:user).permit(:name, :email)
+	end
 
 	def new_user_params
 		params.require(:user).permit(:name, :email, :password)
