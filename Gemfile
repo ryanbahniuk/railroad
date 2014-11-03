6153bc6bfe4fb3261f388f8d0e34efeddcdfc3d0
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2014-11-03 10:24:42 -0600

    removing sqlite from gemfile

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.0'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
 gem 'bson'
 gem 'moped', '~> 2.0.1'
 gem 'mongoid', '~> 4.0.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 PostType.create!(name: "post", aspects: {"title" => "text", "body" => "textarea"})
 PostType.create!(name: "page", aspects: {"title" => "text", "body" => "textarea"})
+User.create!(name: "Ryan Bahniuk", email: "ryan.bahniuk@gmail.com", rights: "admin", password: "password")
